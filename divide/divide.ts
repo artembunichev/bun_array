ce3a85f97e38b6a3691d1365279687c1ce8701cd
spleekz <spleekz@zohomail.com>
2023-06-15 13:06:47 +0300

    divide: handle no find index case

--- divide/divide.ts
+++ divide/divide.ts
@@ -8,10 +8,18 @@ namespace $ {
 	export function $bun_array_divide< T >( arr: Readonly< Array< T > >, divider: ( item: T )=> boolean ): $bun_array_divide_return< T > {
 		var divide_ind = arr.findIndex( divider )
 
-		return {
-			before: arr.slice( 0, divide_ind ),
-			after: arr.slice( divide_ind ),
+		var before: Readonly< Array< T > > = []
+		var after: Readonly< Array< T > > = []
+
+		if ( divide_ind === -1 ) {
+			before = arr
 		}
+		else {
+			before = arr.slice( 0, divide_ind )
+			after = arr.slice( divide_ind )
+		}
+
+		return { before, after }
 	}
 
 }
