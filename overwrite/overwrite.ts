bb9c58390360a7bf3beed30fe7dd6f3c37faabe6
spleekz <spleekz@zohomail.com>
2023-07-02 13:38:50 +0300

    overwrite: simplify

--- overwrite/overwrite.ts
+++ overwrite/overwrite.ts
@@ -1,12 +1,12 @@
 namespace $ {
 
-	export function $bun_array_overwrite< T >( arr: Readonly< Array< T > >, index: number, new_el: T ): Readonly< Array< T > > {
+	export function $bun_array_overwrite< T >( arr: Readonly< Array< T > >, index: number, new_el: T ) {
 
-		var before = arr.slice( 0, index )
+		var res = arr.slice()
 
-		var after = arr.slice( index + 1 )
+		res[ index ] = new_el
 
-		return [ ... before, new_el, ... after ]
+		return res
 
 	}
 
